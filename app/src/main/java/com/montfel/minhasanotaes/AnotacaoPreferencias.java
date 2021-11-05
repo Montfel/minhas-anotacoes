6217b34fc006192e405ae0c7186e295e9c112ebc
Luís Felipe Monteiro <felipemonteirose@gmail.com>
2021-11-05 14:00:09 -0300

    initial commit

---
+++ app/src/main/java/com/montfel/minhasanotaes/AnotacaoPreferencias.java
@@ -0,0 +1,30 @@
+package com.montfel.minhasanotaes;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class AnotacaoPreferencias {
+
+    private final String NOME_ARQUIVO = "anotacao.preferencias";
+    private final String CHAVE_NOME = "nome";
+    private Context context;
+    private SharedPreferences preferences;
+    private SharedPreferences.Editor editor;
+
+    public AnotacaoPreferencias(Context c) {
+        this.context = c;
+        preferences = context.getSharedPreferences(NOME_ARQUIVO, Context.MODE_PRIVATE);
+        editor = preferences.edit();
+    }
+
+    public void salvarAnotacao(String anotacao) {
+        editor.putString(CHAVE_NOME, anotacao);
+        editor.commit();
+
+    }
+
+    public String recuperarAnotacao() {
+        return preferences.getString(CHAVE_NOME, "");
+    }
+
+}
---
+++ app/src/main/java/com/montfel/minhasanotaes/MainActivity.java
@@ -0,0 +1,43 @@
+package com.montfel.minhasanotaes;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.ui.AppBarConfiguration;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.montfel.minhasanotaes.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityMainBinding binding;
+    private AnotacaoPreferencias preferencias;
+    private EditText editAnotacao;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        editAnotacao = findViewById(R.id.editAnotacao);
+        preferencias = new AnotacaoPreferencias(getApplicationContext());
+
+        binding.fab.setOnClickListener(view -> {
+            String textoRecuperado = editAnotacao.getText().toString();
+            if (textoRecuperado.equals("")) {
+                Snackbar.make(view, "Preencha a anotação!", Snackbar.LENGTH_LONG).show();
+            } else {
+                preferencias.salvarAnotacao(textoRecuperado);
+                Snackbar.make(view, "Anotação salva com sucesso!", Snackbar.LENGTH_LONG).show();
+            }
+        });
+
+        String anotacao = preferencias.recuperarAnotacao();
+        if (!anotacao.equals("")){
+            editAnotacao.setText(anotacao);
+        }
+    }
+}
